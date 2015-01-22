26a8c2d73d658ba601e8679ec210a909bd51b0cf
AresTao <wuzhantao0725@163.com>
2015-01-22 13:50:35 +0800

    add db functions

--- webservice/src/com/bupt/turtleservice/action/PolicyAction.java
+++ webservice/src/com/bupt/turtleservice/action/PolicyAction.java
@@ -1,12 +1,17 @@
 package com.bupt.turtleservice.action;
 
 import java.util.ArrayList;
+import java.util.Date;
+
+import net.sf.json.JSONObject;
 
 import org.apache.log4j.Logger;
 
+import com.bupt.turtleservice.db.PolicyDBFunction;
 import com.bupt.turtleservice.db.TransactionException;
 import com.bupt.turtleservice.db.TransactionOperation;
 import com.bupt.turtleservice.model.Policy;
+import com.bupt.turtleservice.utils.DateUtil;
 
 public class PolicyAction {
 
@@ -16,21 +21,42 @@ public class PolicyAction {
 	public PolicyAction() throws TransactionException {
 		this.transactionOperation = new TransactionOperation();
 	}
-	public boolean createPolicy()
+	public boolean createPolicy(JSONObject jsonData) throws Exception
+	{
+		PolicyDBFunction func = new PolicyDBFunction(this.transactionOperation);
+		String name = jsonData.getString("name");
+		String content = jsonData.getString("content");
+		String topic = jsonData.getString("topic");
+		String createTime = jsonData.getString("createTime");
+		String comment = jsonData.getString("comment");
+		func.createPolicy(name, content, topic, createTime, comment);
+		return true;
+	}
+	public boolean updatePolicy(JSONObject jsonData)
 	{
 		return true;
 	}
-	public boolean updatePolicy()
+	public boolean deletePolicy(JSONObject jsonData)
 	{
 		return true;
 	}
-	public boolean deletePolicy()
-	{
-		return true;
-	}
-	public ArrayList<Policy> getPolicy()
+	public ArrayList<Policy> getPolicy(JSONObject jsonData)
 	{
 		return null;
 		
+	}
+	
+	public static void main(String args[]) throws Exception
+	{
+		JSONObject jsonData = new JSONObject();
+		jsonData.accumulate("name", "test");
+		jsonData.accumulate("content", "test");
+		jsonData.accumulate("topic", "test");
+		jsonData.accumulate("createTime", DateUtil.getDatetime(new Date()));
+		jsonData.accumulate("comment", "test");
+		
+		PolicyAction action = new PolicyAction();
+		System.out.println(jsonData.toString());
+		action.createPolicy(jsonData);
 	}
 }
--- webservice/src/com/bupt/turtleservice/db/PolicyDBFunction.java
+++ webservice/src/com/bupt/turtleservice/db/PolicyDBFunction.java
@@ -1,5 +1,7 @@
 package com.bupt.turtleservice.db;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -15,25 +17,37 @@ public class PolicyDBFunction {
 		this.transactionOperation = transactionOperation;
 	}
 	
-	public boolean createPolicy()
+	public boolean createPolicy(String name, String content, String topic, String createTime, String comment) throws Exception
 	{
+		String sql = "INSERT INTO `policy` (`name`, `content`, `topic`, `create_time`, `comment`) VALUES (?,?,?,?,?);";
+		List<Object> values = new ArrayList<Object>();
+		values.add(name);
+		values.add(content);
+		values.add(topic);
+		values.add(createTime);
+		values.add(comment);
 		
+		this.transactionOperation.beginTransaction();
+		ResultSet res = this.transactionOperation.exec(sql, values);
+		this.transactionOperation.commitTransaction();
+	    logger.info("add policy");
 		return true;
 	}
 	
-	public boolean updatePolicy()
+	public boolean updatePolicy(Policy policy)
 	{
 		return true;
 	}
 	
-	public List<Policy> getPolicy()
+	public List<Policy> getPolicy(String policyId)
 	{
 		return null;
 	}
 	
-	public boolean deletePolicy()
+	public boolean deletePolicy(String policyId)
 	{
 		return true;
 	}
 		
+	
 }

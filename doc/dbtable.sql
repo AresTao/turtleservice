7e5edbb2137792aa231e5f5ac76b4a9eef941242
AresTao <wuzhantao0725@163.com>
2015-01-26 13:18:49 +0800

    update db.sql

--- doc/dbtable.sql
+++ doc/dbtable.sql
@@ -7,3 +7,50 @@ CREATE TABLE IF NOT EXISTS `policy` (
     `comment`                           VARCHAR(512)                                         ,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `admin` (
+    `id`                                INT            AUTO_INCREMENT                        ,
+    `name`                              VARCHAR(128)   NOT NULL                              ,
+    `passwd`                            VARCHAR(512)   NOT NULL                              , 
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `user` (
+    `id`                                INT            AUTO_INCREMENT                        ,
+    `name`                              VARCHAR(128)   NOT NULL                              ,
+    `passwd`                            VARCHAR(512)   NOT NULL                              ,
+	`header`                            VARCHAR(128)                                         ,
+    `sex`                               bit                                                  ,
+    `phone`                             VARCHAR(512)                                         ,
+	`isSingle`                          INT                                                  ,
+	`school`                            VARCHAR(128)                                         ,
+	`country`                           VARCHAR(64)                                          ,
+	`sign`                              VARCHAR(128)                                         ,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `topic` (
+    `id`                                INT            AUTO_INCREMENT                        ,
+    `classId`                           INT  NOT NULL                                        ,
+    `title`                             VARCHAR(512)   NOT NULL                              ,
+	`userId`                            INT                                                  ,
+    `description`                       VARCHAR(512)                                         ,
+    `dateTime`                          DATETIME                                             ,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `class` (
+    `id`                                INT            AUTO_INCREMENT                        ,
+	`name`                              VARCHAR(128)   NOT NULL                              ,
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS `reply` (
+    `id`                                INT            AUTO_INCREMENT                        ,
+    `userId`                            INT  NOT NULL                                        ,
+    `topicId`                           VARCHAR(512)   NOT NULL                              ,
+	`dateTime`                          DATETIME                                             ,
+    `message`                           VARCHAR(512)                                         ,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
